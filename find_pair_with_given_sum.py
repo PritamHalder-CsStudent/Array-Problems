900dbdcec208c49f1c88211fffbcb49b6ea7783c
PritamHalder-CsStudent <pritamhalder42002@gmail.com>
2025-12-03 22:09:08 +0530

    update

--- find_pair_with_given_sum.py
+++ find_pair_with_given_sum.py
@@ -1,31 +1,37 @@
-# Find pair with given sum 
-# Example: [1,2,3,4,5], sum=6  --> (1,5),(2,4)
+# Find pair with given sum : Two sum problem
 
+# Given an array of integers nums and an integer target, 
+# return indices of the two numbers such that they add up to target.
+
+# Example
+#Input: nums = [3,2,4], target = 6
+#Output: [1,2] index num of the elements
+
+# time complexity  O(n^2)  Approach:
 def pair(arr,target):
-    ans={}
+    
     for i in range (0,len(arr)):
         for j in range(i+1,len(arr)):
             if arr[i]+arr[j] == target :
-                ans[arr[i]]=arr[j]
-    return ans
+                return [i,j]
+    return []
 
-print(pair([1,2,3,4,5],6))    
-
+print(pair([3,2,4],6))  
+   
 
 # best approach --> time complexity  O(n) and space complexity also O(n) 
 
-def target_pair(arr,target):
-    ans=[]
-    seen=set()
-    for ele in arr:
-        complement=target-ele
-        if complement  in seen:
-            ans.append((ele,complement))
-        seen.add(ele)
-    return ans
+def target_pair(nums,target):
+    map={}
+    for i in range(0,len(nums)):
+        rem=target-nums[i]      # calculating remaining part 
+        if rem in map:      # if rem already present we fount the match element to get target
+            return [map[rem],i]
+        map[nums[i]]=i   # if remaining part not present in the map data structure then add only element to the map 
+    return []        
 
 
-print(target_pair([1,2,3,4,5],6))    
+print(target_pair([3,2,4],6))    
             
             
             
